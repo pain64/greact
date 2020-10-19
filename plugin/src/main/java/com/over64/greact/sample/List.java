79ccc20cf5641c5dd8ac9d24a7915424d7ac9f55
over <you@example.com>
2020-10-19 14:41:02 +0600

    in progress

--- plugin/src/main/java/com/over64/greact/sample/List.java
+++ plugin/src/main/java/com/over64/greact/sample/List.java
@@ -1,17 +1,21 @@
 package com.over64.greact.sample;
 
-import com.over64.greact.model.Slot;
-import com.over64.greact.model.Slot.NoArgs;
 import com.over64.greact.model.Slot.SlotF1;
 import org.over64.jscripter.std.js.DocumentFragment;
 
 import static org.over64.jscripter.std.js.Globals.document;
 
 public class List<T> {
-    public List(DocumentFragment dom, T[] of, Slot<NoArgs, SlotF1<T>> item) {
+    public static class ItemArgs<T> {
+        final SlotF1<T> fn;
+        public ItemArgs(SlotF1<T> fn) {
+            this.fn = fn;
+        }
+    }
+    public List(DocumentFragment dom, T[] of, ItemArgs<T> item) {
         var itemDom = document.createDocumentFragment();
         for (var itemValue : of) {
-            item.f.apply(itemDom, itemValue);
+            item.fn.apply(itemDom, itemValue);
             dom.appendChild(itemDom);
         }
     }
--- plugin/src/main/java/com/over64/greact/sample/Switch.java
+++ plugin/src/main/java/com/over64/greact/sample/Switch.java
@@ -1,6 +1,5 @@
 package com.over64.greact.sample;
 
-import com.over64.greact.model.Slot;
 import com.over64.greact.model.Slot.SlotF0;
 import org.over64.jscripter.std.js.DocumentFragment;
 
@@ -9,18 +8,20 @@ import static org.over64.jscripter.std.js.Globals.document;
 public class Switch<T> {
     public static class CaseArgs<T> {
         final T eq;
+        final SlotF0 fn;
 
-        public CaseArgs(T eq) {
+        public CaseArgs(T eq, SlotF0 fn) {
             this.eq = eq;
+            this.fn = fn;
         }
     }
 
     @SafeVarargs
-    public Switch(DocumentFragment dom, T of, Slot<CaseArgs<T>, SlotF0>... caseIf) {
+    public Switch(DocumentFragment dom, T of, CaseArgs<T>... caseIf) {
         var childDom = document.createDocumentFragment();
         for (var slot : caseIf)
-            if (slot.p.eq.equals(of))
-                slot.f.apply(childDom);
+            if (slot.eq.equals(of))
+                slot.fn.apply(childDom);
 
         dom.appendChild(childDom);
     }
