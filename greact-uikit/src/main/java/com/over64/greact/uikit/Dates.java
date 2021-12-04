7675bdcfc9f01dd66681b41e3a77ecc1eb7136c5
over <overln2@gmail.com@>
2021-12-04 15:28:26 +0600

    add Dates.now()

--- greact-uikit/src/main/java/com/over64/greact/uikit/Dates.java
+++ greact-uikit/src/main/java/com/over64/greact/uikit/Dates.java
@@ -1,29 +1,28 @@
 package com.over64.greact.uikit;
+
 import java.util.Date;
+
 import com.greact.model.JSExpression;
 
 public class Dates {
-
+    public static Date now() {
+        return JSExpression.of("new Date().getTime()");
+    }
     public static Date fromUnixTime(long millis) {
         return JSExpression.of("millis");
     }
-
     public static Date parse(String date) {
         return JSExpression.of("new Date(date).getTime()");
     }
-
     public static long getTime(Date date) {
         return JSExpression.of("date");
     }
-
     public static String toLocaleString(Date date) {
         return JSExpression.of("new Date(date).toLocaleString()");
     }
-
     public static String toLocaleDateString(Date date) {
         return JSExpression.of("new Date(date).toLocaleDateString()");
     }
-
     public static int getYear(java.util.Date date) {
         return JSExpression.of("new Date(date).getFullYear()");
     }
