ae1e6a5b3815b69feaee0269dd5a6de22a675092
Sergey Melnikov <smelnikovimit@gmail.com>
2023-03-23 02:24:38 +0600

    [+] Add Window.open

--- greact/src/main/java/jstack/greact/dom/Window.java
+++ greact/src/main/java/jstack/greact/dom/Window.java
@@ -5,5 +5,5 @@ import jstack.jscripter.transpiler.model.JSNativeAPI;
 @JSNativeAPI public class Window {
     public native double setTimeout(Runnable fn, int delayMillis);
     public native void alert(String text);
-    public native void open(String link, String type);
+    public native void open(String link, String openType);
 }
